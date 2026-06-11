56091077dceb9a95e2f4d6076b065d61671c423d
artem <kebabggbab@gmail.com>
2026-06-11 21:17:33 +0700

    samples(shared): added extensions method to IServiceCollection  for adding view models

---
+++ samples/KebabGGbab.Localization.Samples.Shared/ViewModels/ViewModelExtensions.cs
@@ -0,0 +1,17 @@
+﻿using KebabGGbab.Localization.Samples.Shared.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KebabGGbab.Localization.Samples.Shared.ViewModels
+{
+    public static class ViewModelExtensions
+    {
+        public static IServiceCollection AddViewModels(this IServiceCollection services)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            return services.AddSingleton((s) => new UserViewModel(new User("MyUser")))
+                .AddSingleton<SettingsViewModel>()
+                .AddSingleton<MainViewModel>();
+        }
+    }
+}
