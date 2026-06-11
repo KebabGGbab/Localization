54fc09f93c7a360309cec6cf61d164b40407cfee
artem <kebabggbab@gmail.com>
2026-06-11 21:10:49 +0700

    samples(shared): added User and UserViewModel classes

---
+++ samples/KebabGGbab.Localization.Samples.Shared/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace KebabGGbab.Localization.Samples.Shared.Models
+{
+    public sealed class User
+    {
+        public string Name { get; }
+
+        public User(string name)
+        {
+            ArgumentNullException.ThrowIfNull(name);
+
+            Name = name;
+        }
+    }
+}
---
+++ samples/KebabGGbab.Localization.Samples.Shared/ViewModels/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using KebabGGbab.Localization.Samples.Shared.Models;
+
+namespace KebabGGbab.Localization.Samples.Shared.ViewModels
+{
+    public class UserViewModel : ObservableObject
+    {
+        private readonly User _user;
+
+        public string Name => _user.Name;
+
+        public UserViewModel(User user)
+        {
+            ArgumentNullException.ThrowIfNull(user);
+
+            _user = user;
+        }
+    }
+}
