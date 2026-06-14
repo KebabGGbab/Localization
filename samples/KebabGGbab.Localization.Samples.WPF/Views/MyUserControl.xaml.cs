d7b6e28095abd787899effeb893dff5274c26023
artem <kebabggbab@gmail.com>
2026-06-14 07:44:19 +0700

    samples(wpf): add example localization UserControl

---
+++ samples/KebabGGbab.Localization.Samples.WPF/Views/MyUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace KebabGGbab.Localization.Samples.WPF.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MyUserControl.xaml
+    /// </summary>
+    public partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
