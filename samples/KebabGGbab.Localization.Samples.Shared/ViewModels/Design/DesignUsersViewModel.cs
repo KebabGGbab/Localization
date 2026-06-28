424bc2933a9f76be8e163bb7cd51739f21f66f09
artem <kebabggbab@gmail.com>
2026-06-28 17:24:43 +0700

    feat(samples): add UsersViewModel and vm designer for it

---
+++ samples/KebabGGbab.Localization.Samples.Shared/ViewModels/Design/DesignUsersViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace KebabGGbab.Localization.Samples.Shared.ViewModels.Design
+{
+    public class DesignUsersViewModel : UsersViewModel
+    {
+        public DesignUsersViewModel()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                AddUserCommand.Execute(null);
+            }
+        }
+    }
+}
---
+++ samples/KebabGGbab.Localization.Samples.Shared/ViewModels/UsersViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using KebabGGbab.Localization.Samples.Shared.Models;
+
+namespace KebabGGbab.Localization.Samples.Shared.ViewModels
+{
+    public class UsersViewModel : ObservableObject
+    {
+        private static readonly Random _random = new();
+
+        private readonly ObservableCollection<UserViewModel> _users;
+
+        public ReadOnlyObservableCollection<UserViewModel> Users { get; }
+
+        public IRelayCommand AddUserCommand { get; }
+
+        public IRelayCommand ClearUsersCommand { get; }
+
+        public UsersViewModel()
+        {
+            _users = [];
+            Users = new ReadOnlyObservableCollection<UserViewModel>(_users);
+            AddUserCommand = new RelayCommand(AddUser);
+            ClearUsersCommand = new RelayCommand(ClearUsers);
+        }
+
+        private void AddUser()
+        {
+            int r = _random.Next(1, 4);
+
+            User user = r switch
+            {
+                1 => new User("Artem"),
+                2 => new User("Olya"),
+                _ => new User("Vasya")
+            };
+
+            _users.Add(new UserViewModel(user));
+        }
+
+        private void ClearUsers()
+        {
+            _users.Clear();
+        }
+    }
+}
